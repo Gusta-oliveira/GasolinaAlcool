8c353f81c20ce04afc014be60a9e95f177415f5b
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-06 00:58:19 -0300

    Add project files.

---
+++ GasolinaAlcool/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+float pg, pa, dif;
+
+Console.WriteLine("Informe valor Gasolina: ");
+pg = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe valor Álcool: ");
+pa = float.Parse(Console.ReadLine());
+
+dif = pa / pg;
+if(dif >= 0.72){
+    Console.WriteLine("Compensa gasolina!");
+}
+else
+{
+    Console.WriteLine("Compensa álcool!");
+}
